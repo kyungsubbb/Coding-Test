b0a01c91bc2967c8eea93096c030ecc1d9fa7215
kyungsubbb <isks2322@gmail.com>
2020-11-08 00:20:47 +0900

    1108

---
+++ Baekjoon/Python/1026.py
@@ -0,0 +1,9 @@
+n = int(input())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+answer = 0
+a.sort()
+b.sort(reverse = True)
+for i in range(n) :
+    answer += a[i]*b[i]
+print(answer)
