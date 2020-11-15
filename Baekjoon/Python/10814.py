e3cb3d1130a44063ed32c7cea77cef6b2ee2df99
kyungsubbb <isks2322@gmail.com>
2020-11-15 22:49:18 +0900

    1115

--- Baekjoon/Python/10814.py
+++ Baekjoon/Python/10814.py
@@ -9,3 +9,13 @@ for i in range(N):
 data_list.sort(key=lambda x : int(x[0]) )
 for i in range(len(data_list)) :
     print(data_list[i][0], data_list[i][1])
+
+""" 20201115
+n = int(input())
+arr = []
+for _ in range(n):
+    arr.append(input().split())
+arr.sort(key = lambda x : int(x[0]))
+for i in arr:
+    print(i[0] , i[1])
+"""
