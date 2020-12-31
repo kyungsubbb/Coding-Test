b16faa2782381f72a601ce349606bc24cb7f0d95
kyungsubbb <isks2322@gmail.com>
2020-12-31 21:09:21 +0900

    1231

--- Baekjoon/Python/10845.py
+++ Baekjoon/Python/10845.py
@@ -1,6 +1,62 @@
 import sys
 from collections import deque
 
+
+class queue:
+    def __init__(self):
+        self.queue = deque([])
+    
+    def push(self, num):
+        self.queue.append(num)
+    
+    def pop(self):
+        if self.queue:
+            return self.queue.popleft()
+        else :
+            return -1
+    def size(self):
+        return len(self.queue)
+
+    def empty(self):
+        if len(self.queue) == 0 :
+            return 1
+        else :
+            return 0
+
+    def front(self):
+        if self.queue :
+            return self.queue[0]
+        else :
+            return -1
+            
+    def back(self):
+        if self.queue :
+            return self.queue[-1]
+        else :
+            return -1
+
+
+n = int(input())
+ans = queue()
+for i in range(n):
+    arr = input().split()
+    if arr[0] == "push" :
+        ans.push(arr[1])
+    elif arr[0] == "pop" :
+        print(ans.pop())
+    elif arr[0] == "size" :
+        print(ans.size())
+    elif arr[0] == "empty" :
+        print(ans.empty())
+    elif arr[0] == "front" :
+        print(ans.front())
+    elif arr[0] == "back" :
+        print(ans.back())
+
+'''
+import sys
+from collections import deque
+
 n = int(sys.stdin.readline())
 ans = deque([])
 for i in range(n):
@@ -30,3 +86,5 @@ for i in range(n):
             print(ans[-1])
         else :
             print(-1)
+
+'''
