0bebfe52480b9f1d1fec5077e417bb2f8d2b85fa
kyungsubbb <isks2322@gmail.com>
2021-01-02 01:18:11 +0900

    0102

---
+++ Programmers/Python/문자열 내 p와 y의 개수.py
@@ -0,0 +1,12 @@
+def solution(s):
+    p = 0
+    y = 0
+    for i in s :
+        if i == "p" or i == "P" :
+            p += 1
+        elif i == "y" or i == "Y" :
+            y += 1
+    if p == y :
+        return True
+    else :
+        return False
