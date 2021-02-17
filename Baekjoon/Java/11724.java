ccc55cb0079c014c59d0432828b85717e86595a7
kyungsubbb <isks2322@gmail.com>
2021-02-17 17:55:36 +0900

    0217

---
+++ Baekjoon/Java/11724.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static int count = 0;
+
+	public static void main(String[] args) throws Exception {
+		st = new StringTokenizer(in.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		boolean flag[] = new boolean[N+1];
+		boolean map[][] = new boolean[N+1][N+1];
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(in.readLine(), " ");
+			int tmp1 = Integer.parseInt(st.nextToken());
+			int tmp2 = Integer.parseInt(st.nextToken());
+			map[tmp1][tmp2] = true;
+			map[tmp2][tmp1] = true;
+		}
+		Stack<Integer> stack = new Stack<Integer>();
+		for (int i = 1; i < map.length; i++) {
+			for (int j = 1; j < map[i].length; j++) {
+				boolean check = false;
+				if(i==j) continue;
+				if(map[i][j] && !flag[i]) {
+					stack.push(i);
+					flag[i] = true;
+					while(!stack.isEmpty()) {
+						int tmp = stack.pop();
+						for (int k = 1; k < map[tmp].length; k++) {
+							if(map[tmp][k] && !flag[k]) {
+								stack.push(k);
+								flag[k] = true;
+							}
+						}
+						if(stack.isEmpty()) check = true;
+					}
+					count++;
+				}
+			}
+		}
+		for (int i = 1; i < flag.length; i++) {				// 간선으로 연결되지 않은 정점도 갯수를 세야함!
+			if(!flag[i]) count++;
+		}
+		System.out.println(count);
+	}
+
+}
