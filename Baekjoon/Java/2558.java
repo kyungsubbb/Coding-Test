54d934c83321bdb3fa3440f0142c64eba508300f
SHIN KYUNG SUB <kyungsubbb@gmail.com>
2021-04-29 22:41:05 +0900

    0429

---
+++ Baekjoon/Java/2558.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class BOJ_2558 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println(sc.nextInt() + sc.nextInt());
+	}
+
+}
